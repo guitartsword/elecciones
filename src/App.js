fae908c2c538c4c7544a57883f0fb463d4f8f332
Isaias Valle <guitartsword@gmail.com>
2017-12-03 21:44:23 -0600

    now loads data without clicking

--- src/App.js
+++ src/App.js
@@ -23,38 +23,40 @@ class App extends Component {
     });
     this.handleClick = this.handleClick.bind(this);
   }
+  componentWillMount(){
+    this.handleClick();
+  }
   render() {
     const votos = this.state.votos;
     const loading = this.state.loading;
     const mer = this.state.mer;
     const lastUpdate = this.state.lastUpdate;
     const porcentajeProcesado = mer.procesado/(mer.procesado + mer.noProcesado)*100 || 0
-    const viewData =
-    <div>
-        <ProgressBar
-          titleBar="MER Procesados"
-          leftPercent={porcentajeProcesado}
-          leftText={`${parseFloat(porcentajeProcesado).toFixed(2)}% (${mer.procesado})`}
-          rightPercent={100-porcentajeProcesado}
-          rightText={`${mer.noProcesado}`}
-        />
-        <ProgressBar
-          titleBar="Votos Procesados"
-          leftPercent={100*votos.alianza/votos.total}
-          leftText={`Nasralla: ${parseFloat(100*votos.alianza/votos.total).toFixed(4)}% (${votos.alianza})`}
-          rightPercent={100*votos.nacional/votos.total}
-          rightText={`JOH: ${parseFloat(100*votos.nacional/votos.total).toFixed(4)}% (${votos.nacional})`}
-        />
-        <p>Diferencia: {votos.alianza-votos.nacional} ({parseFloat((votos.alianza-votos.nacional)/votos.total*100).toFixed(4)}%)</p>
-        <p>UltimaActualizacion: {lastUpdate}</p>
-    </div>
-
+    const viewData =(
+      <div>
+          <ProgressBar
+            titleBar="MER Procesados"
+            leftPercent={porcentajeProcesado}
+            leftText={`${parseFloat(porcentajeProcesado).toFixed(2)}% (${mer.procesado})`}
+            rightPercent={100-porcentajeProcesado}
+            rightText={`${mer.noProcesado}`}
+          />
+          <ProgressBar
+            titleBar="Votos Procesados"
+            leftPercent={100*votos.alianza/votos.total}
+            leftText={`Nasralla: ${parseFloat(100*votos.alianza/votos.total).toFixed(4)}% (${votos.alianza})`}
+            rightPercent={100*votos.nacional/votos.total}
+            rightText={`JOH: ${parseFloat(100*votos.nacional/votos.total).toFixed(4)}% (${votos.nacional})`}
+          />
+          <p>Diferencia: {votos.alianza-votos.nacional} ({parseFloat((votos.alianza-votos.nacional)/votos.total*100).toFixed(4)}%)</p>
+          <p>UltimaActualizacion: {lastUpdate}</p>
+      </div>
+    )
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Nasralla | JOH | Diferencia</h1>
         </header>
-        <h2>Haz click en actualizar para traer los datos</h2>
         {votos.total > 0?viewData:''}
         <div>{loading?<pre>Cargando...</pre>:''}</div>
         <button className="button" onClick={this.handleClick}>Actualizar</button>
@@ -62,7 +64,7 @@ class App extends Component {
             tenga en cuenta esto si usa datos (2 clicks es casi 1 mega)
         </p>
         <p className="App-intro">
-          Bienvenidos a la Version 0.1.6
+          Bienvenidos a la Version 0.1.7
         </p>
       </div>
     );
